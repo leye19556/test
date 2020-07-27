8935cf6d88374db7f3d1dbec7769edfea15cd476
leye195 <leye195@naver.com>
2020-07-27 13:34:32 +0900

    test

--- src/db.js
+++ src/db.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const u = process.env.PRODUCTION
-  ? process.env.HEROKU_DB //process.env.PROD_MONGO_DB
+  ? process.env.PROD_MONGO_DB
   : process.env.LOCAL_DB;
 
 mongoose.connect(
-  process.env.PRODUCTION
-    ? process.env.HEROKU_DB /*process.env.PROD_MONGO_DB*/
-    : process.env.LOCAL_DB,
+  process.env.PRODUCTION ? process.env.PROD_MONGO_DB : process.env.LOCAL_DB,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
